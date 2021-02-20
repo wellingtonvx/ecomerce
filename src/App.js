7007c72aa303dd6485faf0cc0f3ba4400ceeb53c
Wellington Ribeiro <wellingtonvx@gmail.com>
2021-02-20 18:23:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import Data from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            Ecomerce
+          </a>
+        </div>
+        <div>
+          <a href="/card">Cart</a>
+          <a href="/singin">Sing In</a>
+        </div>
       </header>
+
+      <main>
+        <div className="row center">
+          {Data.products.map((product) => (
+            <div key={product._id} className="card">
+              <a href={`/product/${product._id}`}>
+                <img
+                  className="medium"
+                  src={product.image}
+                  alt={product.name}
+                />
+              </a>
+              <div className="card-body">
+                <a href={`/product/${product._id}`}>
+                  <h2>{product.name}</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">R${product.price}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+
+      <footer className="row center">all right reserverd</footer>
     </div>
   );
 }
